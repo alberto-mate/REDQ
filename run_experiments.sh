34d48d1ebbe204da784031a82c50798d71869d20
Alberto <100428978@alumnos.uc3m.es>
2024-04-16 18:50:46 +0200

    run experiments

--- run_experiments.sh
+++ run_experiments.sh
@@ -5,60 +5,60 @@
 echo "Running experiments for LunarLander-v2"
 # SAC with alpha = 0.05
 echo "Running SAC with alpha = 0.05"
-python main.py --env LunarLander-v2 --kwargs N=2 G=1 M=2 alpha=0.05 --exp_name SAC_alpha0.05 --total_timesteps 200000 --seed 1
-python main.py --env LunarLander-v2 --kwargs N=2 G=1 M=2 alpha=0.05 --exp_name SAC_alpha0.05 --total_timesteps 200000 --seed 2
-python main.py --env LunarLander-v2 --kwargs N=2 G=1 M=2 alpha=0.05 --exp_name SAC_alpha0.05 --total_timesteps 200000 --seed 3
-python main.py --env LunarLander-v2 --kwargs N=2 G=1 M=2 alpha=0.05 --exp_name SAC_alpha0.05 --total_timesteps 200000 --seed 4
+python main.py --kwargs N=2 G=1 M=2 alpha=0.05 --exp_name SAC_alpha0.05 --total_timesteps 200000 --seed 1 --env LunarLander-v2
+python main.py --kwargs N=2 G=1 M=2 alpha=0.05 --exp_name SAC_alpha0.05 --total_timesteps 200000 --seed 2 --env LunarLander-v2
+python main.py --kwargs N=2 G=1 M=2 alpha=0.05 --exp_name SAC_alpha0.05 --total_timesteps 200000 --seed 3 --env LunarLander-v2
+python main.py --kwargs N=2 G=1 M=2 alpha=0.05 --exp_name SAC_alpha0.05 --total_timesteps 200000 --seed 4 --env LunarLander-v2
 
 # # REDQ with alpha = 0.05 and N=5, G=5, M=2
 echo "Running REDQ with alpha = 0.05 and N=5, G=5, M=2"
-python main.py --env LunarLander-v2 --kwargs N=5 G=5 M=2 alpha=0.05 --exp_name REDQ_alpha0.05_N5_G5_M2 --total_timesteps 200000 --seed 1
-python main.py --env LunarLander-v2 --kwargs N=5 G=5 M=2 alpha=0.05 --exp_name REDQ_alpha0.05_N5_G5_M2 --total_timesteps 200000 --seed 2
+python main.py --kwargs N=5 G=5 M=2 alpha=0.05 --exp_name REDQ_alpha0.05_N5_G5_M2 --total_timesteps 200000 --seed 1 --env LunarLander-v2
+python main.py --kwargs N=5 G=5 M=2 alpha=0.05 --exp_name REDQ_alpha0.05_N5_G5_M2 --total_timesteps 200000 --seed 2 --env LunarLander-v2
 
 # REDQ with alpha = 0.2 and N=5, G=5, M=2
 echo "Running REDQ with alpha = 0.2 and N=5, G=5, M=2"
-python main.py --env LunarLander-v2 --kwargs N=5 G=5 M=2 alpha=0.2 --exp_name REDQ_alpha0.2_N5_G5_M2 --total_timesteps 200000 --seed 1
-python main.py --env LunarLander-v2 --kwargs N=5 G=5 M=2 alpha=0.2 --exp_name REDQ_alpha0.2_N5_G5_M2 --total_timesteps 200000 --seed 2
+python main.py --kwargs N=5 G=5 M=2 alpha=0.2 --exp_name REDQ_alpha0.2_N5_G5_M2 --total_timesteps 200000 --seed 1 --env LunarLander-v2
+python main.py --kwargs N=5 G=5 M=2 alpha=0.2 --exp_name REDQ_alpha0.2_N5_G5_M2 --total_timesteps 200000 --seed 2 --env LunarLander-v2
 
 # SAC with alpha = 0.2
 echo "Running SAC with alpha = 0.2"
-python main.py --env LunarLander-v2 --kwargs N=2 G=1 M=2 alpha=0.2 --exp_name SAC_alpha0.2 --total_timesteps 200000 --seed 42
-python main.py --env LunarLander-v2 --kwargs N=2 G=1 M=2 alpha=0.2 --exp_name SAC_alpha0.2 --total_timesteps 200000 --seed 43
-python main.py --env LunarLander-v2 --kwargs N=2 G=1 M=2 alpha=0.2 --exp_name SAC_alpha0.2 --total_timesteps 200000 --seed 44
-python main.py --env LunarLander-v2 --kwargs N=2 G=1 M=2 alpha=0.2 --exp_name SAC_alpha0.2 --total_timesteps 200000 --seed 45
+python main.py --kwargs N=2 G=1 M=2 alpha=0.2 --exp_name SAC_alpha0.2 --total_timesteps 200000 --seed 42 --env LunarLander-v2
+python main.py --kwargs N=2 G=1 M=2 alpha=0.2 --exp_name SAC_alpha0.2 --total_timesteps 200000 --seed 43 --env LunarLander-v2
+python main.py --kwargs N=2 G=1 M=2 alpha=0.2 --exp_name SAC_alpha0.2 --total_timesteps 200000 --seed 44 --env LunarLander-v2
+python main.py --kwargs N=2 G=1 M=2 alpha=0.2 --exp_name SAC_alpha0.2 --total_timesteps 200000 --seed 45 --env LunarLander-v2
 
 # REDQ with alpha = 0.05 and N=3, G=10, M=2
 echo "Running REDQ with alpha = 0.05 and N=3, G=10, M=2"
-python main.py --env LunarLander-v2 --kwargs N=3 G=10 M=2 alpha=0.05 --exp_name REDQ_alpha0.05_N3_G10_M2 --total_timesteps 200000 --seed 321
-python main.py --env LunarLander-v2 --kwargs N=3 G=10 M=2 alpha=0.05 --exp_name REDQ_alpha0.05_N3_G10_M2 --total_timesteps 200000 --seed 32
+python main.py --kwargs N=3 G=10 M=2 alpha=0.05 --exp_name REDQ_alpha0.05_N3_G10_M2 --total_timesteps 200000 --seed 321 --env LunarLander-v2
+python main.py --kwargs N=3 G=10 M=2 alpha=0.05 --exp_name REDQ_alpha0.05_N3_G10_M2 --total_timesteps 200000 --seed 32 --env LunarLander-v2
 
 ## BipedalWalker-v3
 echo "Running experiments for BipedalWalker-v3"
 # SAC with alpha = 0.05
 echo "Running SAC with alpha = 0.05"
-python main.py --env BipedalWalker-v3 --kwargs N=2 G=1 M=2 alpha=0.05 --exp_name SAC_alpha0.05 --total_timesteps 500000 --seed 1 # done
-python main.py --env BipedalWalker-v3 --kwargs N=2 G=1 M=2 alpha=0.05 --exp_name SAC_alpha0.05 --total_timesteps 500000 --seed 2
-python main.py --env BipedalWalker-v3 --kwargs N=2 G=1 M=2 alpha=0.05 --exp_name SAC_alpha0.05 --total_timesteps 500000 --seed 3
-python main.py --env BipedalWalker-v3 --kwargs N=2 G=1 M=2 alpha=0.05 --exp_name SAC_alpha0.05 --total_timesteps 500000 --seed 4
+python main.py --kwargs N=2 G=1 M=2 alpha=0.05 --exp_name SAC_alpha0.05 --total_timesteps 500000 --seed 1 --env BipedalWalker-v3
+python main.py --kwargs N=2 G=1 M=2 alpha=0.05 --exp_name SAC_alpha0.05 --total_timesteps 500000 --seed 2 --env BipedalWalker-v3
+python main.py --kwargs N=2 G=1 M=2 alpha=0.05 --exp_name SAC_alpha0.05 --total_timesteps 500000 --seed 3 --env BipedalWalker-v3
+python main.py --kwargs N=2 G=1 M=2 alpha=0.05 --exp_name SAC_alpha0.05 --total_timesteps 500000 --seed 4 --env BipedalWalker-v3
 
 # REDQ with alpha = 0.05 and N=5, G=5, M=2
 echo "Running REDQ with alpha = 0.05 and N=5, G=5, M=2"
-python main.py --env BipedalWalker-v3 --kwargs N=5 G=5 M=2 alpha=0.05 --exp_name REDQ_alpha0.05_N5_G5_M2 --total_timesteps 500000 --seed 1
-python main.py --env BipedalWalker-v3 --kwargs N=5 G=5 M=2 alpha=0.05 --exp_name REDQ_alpha0.05_N5_G5_M2 --total_timesteps 500000 --seed 2
+python main.py --kwargs N=5 G=5 M=2 alpha=0.05 --exp_name REDQ_alpha0.05_N5_G5_M2 --total_timesteps 500000 --seed 1 --env BipedalWalker-v3
+python main.py --kwargs N=5 G=5 M=2 alpha=0.05 --exp_name REDQ_alpha0.05_N5_G5_M2 --total_timesteps 500000 --seed 2 --env BipedalWalker-v3
 
 # REDQ with alpha = 0.2 and N=5, G=5, M=2
 echo "Running REDQ with alpha = 0.2 and N=5, G=5, M=2"
-python main.py --env BipedalWalker-v3 --kwargs N=5 G=5 M=2 alpha=0.2 --exp_name REDQ_alpha0.2_N5_G5_M2 --total_timesteps 500000 --seed 1
-python main.py --env BipedalWalker-v3 --kwargs N=5 G=5 M=2 alpha=0.2 --exp_name REDQ_alpha0.2_N5_G5_M2 --total_timesteps 500000 --seed 2
+python main.py --kwargs N=5 G=5 M=2 alpha=0.2 --exp_name REDQ_alpha0.2_N5_G5_M2 --total_timesteps 500000 --seed 1 --env BipedalWalker-v3
+python main.py --kwargs N=5 G=5 M=2 alpha=0.2 --exp_name REDQ_alpha0.2_N5_G5_M2 --total_timesteps 500000 --seed 2 --env BipedalWalker-v3
 
 # SAC with alpha = 0.2
 echo "Running SAC with alpha = 0.2"
-python main.py --env BipedalWalker-v3 --kwargs N=2 G=1 M=2 alpha=0.2 --exp_name SAC_alpha0.2 --total_timesteps 500000 --seed 42
-python main.py --env BipedalWalker-v3 --kwargs N=2 G=1 M=2 alpha=0.2 --exp_name SAC_alpha0.2 --total_timesteps 500000 --seed 43
-python main.py --env BipedalWalker-v3 --kwargs N=2 G=1 M=2 alpha=0.2 --exp_name SAC_alpha0.2 --total_timesteps 500000 --seed 44
-python main.py --env BipedalWalker-v3 --kwargs N=2 G=1 M=2 alpha=0.2 --exp_name SAC_alpha0.2 --total_timesteps 500000 --seed 45
+python main.py --kwargs N=2 G=1 M=2 alpha=0.2 --exp_name SAC_alpha0.2 --total_timesteps 500000 --seed 42 --env BipedalWalker-v3
+python main.py --kwargs N=2 G=1 M=2 alpha=0.2 --exp_name SAC_alpha0.2 --total_timesteps 500000 --seed 43 --env BipedalWalker-v3
+python main.py --kwargs N=2 G=1 M=2 alpha=0.2 --exp_name SAC_alpha0.2 --total_timesteps 500000 --seed 44 --env BipedalWalker-v3
+python main.py --kwargs N=2 G=1 M=2 alpha=0.2 --exp_name SAC_alpha0.2 --total_timesteps 500000 --seed 45 --env BipedalWalker-v3
 
 # REDQ with alpha = 0.05 and N=3, G=10, M=2
 echo "Running REDQ with alpha = 0.05 and N=3, G=10, M=2"
-python main.py --env BipedalWalker-v3 --kwargs N=3 G=10 M=2 alpha=0.05 --exp_name REDQ_alpha0.05_N3_G10_M2 --total_timesteps 500000 --seed 321
-python main.py --env BipedalWalker-v3 --kwargs N=3 G=10 M=2 alpha=0.05 --exp_name REDQ_alpha0.05_N3_G10_M2 --total_timesteps 500000 --seed 32
+python main.py --kwargs N=3 G=10 M=2 alpha=0.05 --exp_name REDQ_alpha0.05_N3_G10_M2 --total_timesteps 500000 --seed 321 --env BipedalWalker-v3
+python main.py --kwargs N=3 G=10 M=2 alpha=0.05 --exp_name REDQ_alpha0.05_N3_G10_M2 --total_timesteps 500000 --seed 32 --env BipedalWalker-v3
